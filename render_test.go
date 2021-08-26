a155eff6871d5264817882613db99d95e672bbc1
Dakota Walsh <kota@nilsu.org>
2021-08-26 15:55:22 +1200

    implement table-driven tests

--- render_test.go
+++ render_test.go
@@ -13,39 +13,55 @@ import (
 	"github.com/yuin/goldmark/util"
 )
 
-func TestRender(t *testing.T) {
-	src, err := os.ReadFile("test_data/render.md")
+func run(srcPath string, wantPath string, config Config) (error, []byte, []byte) {
+	src, err := os.ReadFile(srcPath)
 	if err != nil {
-		t.Fatal(err)
+		return err, nil, nil
 	}
-	want, err := os.ReadFile("test_data/render.gmi")
+	want, err := os.ReadFile(wantPath)
 	if err != nil {
-		t.Fatal(err)
+		return err, nil, nil
 	}
 	var buf bytes.Buffer
-
-	config := NewConfig()
 	md := goldmark.New(
 		goldmark.WithExtensions(
 			extension.Linkify,
 			extension.Strikethrough,
 		),
 	)
-	ar := NewGemRenderer(config)
+	ar := NewGemRenderer(&config)
 	md.SetRenderer(
 		renderer.NewRenderer(
 			renderer.WithNodeRenderers(util.Prioritized(ar, 1000))))
 
 	if err := md.Convert(src, &buf); err != nil {
-		t.Fatal(err)
+		return err, nil, nil
+	}
+	got := buf.Bytes()
+	return nil, want, got
+}
+
+func TestRender(t *testing.T) {
+	var tests = []struct {
+		srcPath  string
+		wantPath string
+		config   Config
+	}{
+		{"test_data/render.md", "test_data/renderDefault.gmi",
+			Config{HeadingLinkAuto, ParagraphLinkBelow, EmphasisOff, StrikethroughOff, CodeSpanOff}},
 	}
 
-	got := buf.Bytes()
-	if !cmp.Equal(got, want) {
-		err := os.WriteFile("fail.gmi", got, 0644)
+	for _, test := range tests {
+		err, want, got := run(test.srcPath, test.wantPath, test.config)
 		if err != nil {
 			t.Fatal(err)
 		}
-		t.Fatal(fmt.Println(cmp.Diff(got, want)))
+		if !cmp.Equal(got, want) {
+			err := os.WriteFile("fail.gmi", got, 0644)
+			if err != nil {
+				t.Fatal(err)
+			}
+			t.Fatal(fmt.Println(cmp.Diff(got, want)))
+		}
 	}
 }
